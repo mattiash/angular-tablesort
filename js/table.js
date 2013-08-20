3bc593e3fe24121f0f6679561101150f75fc203b
Mattias Holmlund <mattias.git@holmlund.se>
2013-08-20 19:52:12 +0200

    Feature complete

--- js/table.js
+++ js/table.js
@@ -1,9 +1,12 @@
 var myApp = angular.module( 'myApp', ['tableSort' ] );
 
 function ShoppingCartCtrl($scope)  {
-    
+
         $scope.items = [
 			{Name: "Soap", Price: "25", Quantity: "10"},
+			{Name: "Shaving cream", Price: "50", Quantity: "15"},
+	                {Name: "Shampoo", Price: "100", Quantity: "5"},
+			{Name: "Soap", Price: "125", Quantity: "10"},
 			{Name: "Shaving cream", Price: "50", Quantity: "15"},
 			{Name: "Shampoo", Price: "100", Quantity: "5"}
 		];
@@ -11,44 +14,111 @@ function ShoppingCartCtrl($scope)  {
 
 var tableSortModule = angular.module( 'tableSort', [] );
 
-tableSortModule.directive('sorttable', function() {
+tableSortModule.directive('sorttable', function( $log ) {
     return {
 	scope: true,
 	controller: function($scope) {
-	    $scope.sortExpression = "";
+	    $scope.sortExpression = [];
 	    $scope.headings = [];
-	    this.setSortField = function( fieldname, element ) {
+
+	  var parse_sortexpr = function( expr ) {
+	    var e = expr.split(" ");
+	    if( e.length > 2 ) {
+	      $log.error( "tablesorter: Invalid sort expression " + expr );
+	      return [null,null, false];
+	    }
+	    else if( e.length === 2 ) {
+	      e[1] = eval(e[1]);
+	      return e.concat([false]);
+	    }
+	    else {
+	      return e.concat( [null, false] );
+	    }
+	  };
+
+	  this.setSortField = function( sortexpr, element ) {
 		var i;
-		for( i=0; i<$scope.headings.length; i=i+1 ) {
-		    $scope.headings[i].removeClass( "sorttable-desc" );
+	    var expr = parse_sortexpr( sortexpr );
+	      if( $scope.sortExpression.length === 1 && $scope.sortExpression[0][0] === expr[0] ) {
+		if( $scope.sortExpression[0][2] ) {
+		  element.removeClass( "sorttable-desc" );
+		  element.addClass( "sorttable-asc" );
+		  $scope.sortExpression[0][2] = false;
 		}
-		element.addClass( "sorttable-desc" );
-		$scope.sortExpression = fieldname;
+		else {
+		  element.removeClass( "sorttable-asc" );
+		  element.addClass( "sorttable-desc" );
+		  $scope.sortExpression[0][2] = true;
+		}
+	      }
+	      else {
+		for( i=0; i<$scope.headings.length; i=i+1 ) {
+		    $scope.headings[i]
+		      .removeClass( "sorttable-desc" )
+		      .removeClass( "sorttable-asc" );
+		}
+		element.addClass( "sorttable-asc" );
+  		$scope.sortExpression = [expr];
+	      }
 		$scope.$apply();
 	    };
 
-	    this.addSortField = function( fieldname, element ) {
-		element.addClass( "sorttable-desc" );
-		$scope.sortExpression = fieldname;
-		$scope.$apply();
+	    this.addSortField = function( sortexpr, element ) {
+	      var i;
+	      var toggle_order = false;
+              var expr = parse_sortexpr( sortexpr )
+		for( i=0; i<$scope.sortExpression.length; i=i+1 ) {
+		  if( $scope.sortExpression[i][0] === expr[0] ) {
+		    if( $scope.sortExpression[i][2] ) {
+		      element.removeClass( "sorttable-desc" );
+		      element.addClass( "sorttable-asc" );
+		      $scope.sortExpression[i][2] = false;
+		    }
+		    else {
+		      element.removeClass( "sorttable-asc" );
+		      element.addClass( "sorttable-desc" );
+		      $scope.sortExpression[i][2] = true;
+		    }
+		    toggle_order = true;
+		  }
+		}
+	      if( !toggle_order ) {
+	        element.addClass( "sorttable-asc" );
+	        $scope.sortExpression.push( expr );
+	      }
+              $scope.$apply();
 	    };
 
 	    this.registerHeading = function( headingelement ) {
 		$scope.headings.push( headingelement );
 	    };
 
-	    $scope.mySortFunction = function(item) {
-		if(isNaN(item[$scope.sortExpression]))
-		    return item[$scope.sortExpression];
-		return parseInt(item[$scope.sortExpression]);
-	    }
-	    
 	    $scope.sortFun = function( a, b ) {
-		return a[$scope.sortExpression] > b[$scope.sortExpression] ? 1 : -1;
+	      var i, aval, bval, descending;
+	      for( i=0; i<$scope.sortExpression.length; i=i+1 ){
+		aval = a[$scope.sortExpression[i][0]];
+		bval = b[$scope.sortExpression[i][0]];
+		if( $scope.sortExpression[i][1] ) {
+		  aval = $scope.sortExpression[i][1]( aval );
+		  bval = $scope.sortExpression[i][1]( bval );
+		}
+		filterFun = b[$scope.sortExpression[i][1]];
+	        if( filterFun ) {
+		  aval = filterFun( aval );
+		  bval = filterFun( bval );
+		}
+		descending = $scope.sortExpression[i][2];
+		if( aval > bval ) {
+		  return descending ? -1 : 1;
+		}
+		else if( aval < bval ) {
+		  return descending ? 1 : -1;
+		}
+	      }
+	      return 0;
 	    };
-	    
 	}
-    }
+    };
 } );
 
 tableSortModule.directive('sort', function() {
@@ -66,7 +136,7 @@ tableSortModule.directive('sort', function() {
 	    element.bind('click', clickingCallback);
 	    sortTableCtrl.registerHeading( element );
 	}
-    } 
+    }
 });
 
 tableSortModule.directive("sorttablerepeat", function() {
@@ -74,7 +144,6 @@ tableSortModule.directive("sorttablerepeat", function() {
         priority: 2000,
         compile: function(tElement, tAttrs, transclude) {
             tAttrs.ngRepeat += " | tablesortOrderBy:sortFun";
-            console.log(tAttrs);
         }
     };
 } );
@@ -83,9 +152,6 @@ tableSortModule.filter( 'tablesortOrderBy', function( $parse ){
     return function(array, sortfun ) {
 	var arrayCopy = [];
 	for ( var i = 0; i < array.length; i++) { arrayCopy.push(array[i]); }
-	
-//	console.log(expr);
-	
 	return arrayCopy.sort( sortfun );
-  } 
+    };
 } );
