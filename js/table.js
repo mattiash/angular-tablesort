f1ea8792207eb5753a2b79a46f5e3d72f42a7533
Mattias Holmlund <mattias.git@holmlund.se>
2013-08-21 20:02:52 +0200

    Improved example

--- js/table.js
+++ js/table.js
@@ -1,12 +1,12 @@
 var myApp = angular.module( 'myApp', ['tableSort' ] );
 
-function ShoppingCartCtrl($scope)  {
+function tableTestCtrl($scope)  {
     $scope.items = [
-	{Name: "Soap", Price: "25", Quantity: "10"},
-	{Name: "Shaving cream", Price: "50", Quantity: "15"},
-	{Name: "Shampoo", Price: "100", Quantity: "5"},
-	{Name: "Soap", Price: "125", Quantity: "10"},
-	{Name: "Shaving cream", Price: "50", Quantity: "15"},
-	{Name: "Shampoo", Price: "100", Quantity: "5"}
+	{Id: "01", Name: "A", Price: "1.00", Quantity: "1"},
+	{Id: "02", Name: "B", Price: "10.00", Quantity: "1"},
+	{Id: "04", Name: "C", Price: "9.50", Quantity: "10"},
+	{Id: "03", Name: "a", Price: "9.00", Quantity: "2"},
+	{Id: "06", Name: "b", Price: "100.00", Quantity: "2"},
+	{Id: "05",Name: "c", Price: "1.20", Quantity: "2"},
     ];
 }
